4848039089838ec25d1c7a8097e8b7294739b115
Amandeep Singh <amandeep90s@users.noreply.github.com>
2025-10-22 20:52:38 +0530

    feat: Add ESLint and Prettier configuration, update project settings, and format code

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
-import path from "path";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
   turbopack: {
-    root: path.join(__dirname, ".."),
+    root: path.join(__dirname, '..'),
   },
 };
 
